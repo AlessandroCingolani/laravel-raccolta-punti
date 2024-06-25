dca79dac7d192e2ad6f16701ffb3edf434ba8244
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-06-25 12:05:34 +0200

    [add]:link to add purchase from customer

--- resources/views/admin/customers/show.blade.php
+++ resources/views/admin/customers/show.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.admin')
 @section('content')
     <h2>CLIENTE {{ $customer->name }}</h2>
-
+    <div class="mb-3">
+        <a href="{{ route('admin.selected-client', $customer) }}">Aggiungi nuovo acquisto
+        </a>
+    </div>
     <ul>
         @forelse ($purchases as $purchase)
             <li>ID:{{ $purchase->id }} Amount: €{{ $purchase->amount }}</li>
