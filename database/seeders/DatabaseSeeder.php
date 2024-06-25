17e9cfe42e9d0fcc600a331717fa0301b4dd3e25
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-06-25 12:04:09 +0200

    [add]:default user seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            DefaultUserSeeder::class,
             CustomersSeeder::class,
             Loyalty_PointsSeeder::class,
             PurchasesSeeder::class,
