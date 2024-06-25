c746aaa59cb375d88acbfa6024d53229589b05b9
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-06-25 12:02:23 +0200

    [add]: helper formatted date

--- app/Functions/Helper.php
+++ app/Functions/Helper.php
@@ -10,4 +10,10 @@ class Helper
         $money_for_point = 10;
         return floor($price / $money_for_point);
     }
+
+    public static function formatDate($date)
+    {
+        $new_date = date_create($date);
+        return date_format($new_date, 'd/m/Y');
+    }
 }
