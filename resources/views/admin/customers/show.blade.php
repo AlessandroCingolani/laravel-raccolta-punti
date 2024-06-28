9f1740545eb44856e99e1114ec5f0c7d8ed3f7b9
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-06-28 17:00:31 +0200

    [add]:container fluid

--- resources/views/admin/customers/show.blade.php
+++ resources/views/admin/customers/show.blade.php
@@ -1,18 +1,21 @@
 @extends('layouts.admin')
 @section('content')
-    <h2>CLIENTE {{ $customer->name }}</h2>
-    <div class="mb-3">
-        <a href="{{ route('admin.purchases.create', ['id' => $customer]) }}">Aggiungi nuovo acquisto
-        </a>
-    </div>
-    <h1 class="text-success">PUNTI TOTALI : {{ $customer->customer_points }}</h1>
-    <h1 class="text-success">SOLDI SPESI : {{ $amount }} €</h1>
+    <div class="container-fluid">
 
-    <ul>
-        @forelse ($purchases as $purchase)
-            <li>ID:{{ $purchase->id }} Amount: €{{ $purchase->amount }}</li>
-        @empty
-            <li>Nessun acquisto</li>
-        @endforelse
-    </ul>
+        <h2>CLIENTE {{ $customer->name }}</h2>
+        <div class="mb-3">
+            <a href="{{ route('admin.purchases.create', ['id' => $customer]) }}">Aggiungi nuovo acquisto
+            </a>
+        </div>
+        <h1 class="text-success">PUNTI TOTALI : {{ $customer->customer_points }}</h1>
+        <h1 class="text-success">SOLDI SPESI : {{ $amount }} €</h1>
+
+        <ul>
+            @forelse ($purchases as $purchase)
+                <li>ID:{{ $purchase->id }} Amount: €{{ $purchase->amount }}</li>
+            @empty
+                <li>Nessun acquisto</li>
+            @endforelse
+        </ul>
+    </div>
 @endsection
