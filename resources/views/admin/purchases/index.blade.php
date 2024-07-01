eb831edd89d641cb283ae24813f15ebe7dde5b8a
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-07-01 14:32:55 +0200

    [add]:table to display latest purchases

--- resources/views/admin/purchases/index.blade.php
+++ resources/views/admin/purchases/index.blade.php
@@ -12,7 +12,7 @@ use App\Functions\Helper;
 
     @dump($purchases)
     {{-- TODO: suddividere acquisti per mese settimana o anno --}}
-    <ul>
+    {{-- <ul>
         @forelse ($purchases as $purchase)
             <li>ID:{{ $purchase->id }} DATA:{{ Helper::formatDate($purchase->created_at) }} IMPORTO:
                 €{{ $purchase->amount }} <a href="{{ route('admin.purchases.edit', $purchase) }}" class="btn btn-warning">
@@ -30,7 +30,57 @@ use App\Functions\Helper;
         @empty
             <li>Nessun acquisto</li>
         @endforelse
-    </ul>
+    </ul> --}}
+    <table class="table table-bordered border-success">
+        <thead class="table-success">
+            <tr>
+                <th scope="col">
+                    ID Acquisto
+                </th>
+                <th scope="col">
+                    Nome cliente
+                </th>
+                <th scope="col">
+                    Data Acquisto
+                </th>
+                <th scope="col">
+                    Importo
+                </th>
+
+                <th scope="col">
+                    Azioni
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($purchases as $purchase)
+                <tr>
+                    <td>{{ $purchase->id }}</td>
+                    <td>{{ $purchase->customer?->name }}</td>
+                    <td>{{ Helper::formatDate($purchase->created_at) }}</td>
+                    <td>€{{ $purchase->amount }}</td>
+
+                    <td class="text-center">
+                        <a href="{{ route('admin.purchases.edit', $purchase) }}" class="btn btn-warning">
+                            <i class="fa-solid fa-pencil "></i>
+                        </a>
+                        <form class="d-inline-block" action="{{ route('admin.purchases.destroy', $purchase) }}"
+                            method="POST" onsubmit=" return confirm('Sei sicuro di voler cancellare questo acquisto?')">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">
+                                <i class="fa-regular fa-trash-can"></i>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @empty
+                <li>Nessun acquisto</li>
+            @endforelse
+        </tbody>
+    </table>
+
+
     <div class="paginator w-50">
         {{ $purchases->links() }}
     </div>
