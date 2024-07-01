c0d64833433e03ff9868274ff46393de90dd4d96
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-07-01 17:30:08 +0200

    [add]:email controller test mailtrap send email

---
+++ app/Http/Controllers/Admin/EmailController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Mail\CouponsAvailable;
+use App\Models\Lead;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Functions\Helper;
+
+class EmailController extends Controller
+{
+    public function send(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make(
+            $data,
+            [
+                'name' => 'required|string|min:2| max:50',
+                'email' => 'required|email',
+                'customer_points' => 'required|int'
+
+            ],
+            [
+                'name.required' => 'Il nome è richiesto',
+                'name.string' => 'Il nome deve essere una stringa',
+                'name.min' => 'Il nome deve avere almeno 2 caratteri',
+                'name.max' => 'Il nome non può avere più di 50 caratteri',
+                'email.required' => 'Email is required',
+                'customer_points.required' => 'Coupons are required',
+                'customer_points.int' => 'Coupons must be int'
+            ]
+        );
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            dd($errors);
+            return redirect()->route('admin.customers.index')->with($errors);
+        }
+
+        // Create Lead and fill with datas from input
+        $new_lead = new Lead();
+        $new_lead->name = $data['name'];
+        $new_lead->name = $data['email'];
+        $new_lead->customer_points = Helper::discountCoupons($data['customer_points']) > 0 ? Helper::discountCoupons($data['customer_points']) : 0;
+
+
+        // Invia l'email
+        Mail::to($data['email'])->send(new CouponsAvailable($new_lead));
+        return redirect()->route('admin.customers.index')->with('success', 'Email inviata  con successo');
+    }
+}
