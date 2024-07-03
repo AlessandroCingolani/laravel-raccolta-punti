5f2725602b72525911018935587eee11b1335fa8
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-07-03 16:11:43 +0200

    [add]:input label form city and address

--- resources/views/admin/customers/create-edit.blade.php
+++ resources/views/admin/customers/create-edit.blade.php
@@ -22,7 +22,7 @@
                     @method($method)
 
                     <div class="mb-3">
-                        <label for="name" class="form-label">Nome Cliente *</label>
+                        <label for="name" class="form-label">Nome e Cognome *</label>
                         <input id="name" class="form-control @error('name') is-invalid @enderror" name="name"
                             value="{{ old('name', $customer?->name) }}" autocomplete="name" type="text">
                         @error('name')
@@ -31,7 +31,7 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="email" class="form-label">Email Cliente *</label>
+                        <label for="email" class="form-label">Email *</label>
                         <input id="email" class="form-control @error('email') is-invalid @enderror" name="email"
                             value="{{ old('email', $customer?->email) }}" autocomplete="email" type="text">
 
@@ -41,10 +41,28 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="phone" class="form-label">Telefono Cliente </label>
+                        <label for="phone" class="form-label">Telefono </label>
                         <input id="phone" class="form-control @error('phone') is-invalid @enderror" name= "phone"
                             value="{{ old('phone', $customer?->phone) }}" autocomplete="phone" type="text">
                         @error('phone')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="address" class="form-label">Indirizzo </label>
+                        <input id="address" class="form-control @error('address') is-invalid @enderror" name= "address"
+                            value="{{ old('address', $customer?->address) }}" autocomplete="address" type="text">
+                        @error('address')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="city" class="form-label">Città </label>
+                        <input id="city" class="form-control @error('city') is-invalid @enderror" name= "city"
+                            value="{{ old('city', $customer?->city) }}" autocomplete="city" type="text">
+                        @error('city')
                             <p class="text-danger">{{ $message }}</p>
                         @enderror
                     </div>
