f297584b64ac74a1bdc45001ff963b3bf18d9d16
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-10-23 14:29:14 +0200

    [add]:table gift vouchers

--- resources/views/admin/gift_vouchers/index.blade.php
+++ resources/views/admin/gift_vouchers/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.admin')
 
+
 @section('content')
     <div class="container-fluid p-3">
         @if (session('success'))
@@ -14,16 +15,72 @@
                         <li>{{ $error }}</li>
                     @endforeach
                 </ul>
-
             </div>
         @endif
+
         @if (count($vouchers) > 0)
             <div class="d-flex justify-content-between">
                 <h2 class="mb-3">Lista buoni regalo</h2>
             </div>
-            @foreach ($vouchers as $voucher)
-                <p>{{ $voucher }}</p>
-            @endforeach
+            <table class="table table-bordered border-info">
+                <thead class="table-info">
+                    <tr>
+                        <th scope="col">
+                            ID
+                        </th>
+                        <th scope="col">
+                            Destinatario
+                        </th>
+                        <th scope="col">
+                            Codice Voucher
+                        </th>
+                        <th scope="col">
+                            Importo
+                        </th>
+                        <th scope="col">
+                            Data di Scadenza
+                        </th>
+                        <th scope="col">
+                            Stato
+                        </th>
+                        <th scope="col">
+                            Azioni
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($vouchers as $voucher)
+                        <tr>
+                            <td>{{ $voucher->id }}</td>
+                            <td>{{ $voucher->recipient_first_name . ' ' . $voucher->recipient_last_name }}</td>
+                            <td>{{ $voucher->code }}</td>
+                            <td>{{ $voucher->amount }} €</td>
+                            <td>{{ $voucher->expiration_date }}</td>
+                            <td>
+                                @if ($voucher->used_at)
+                                    Usato il {{ $voucher->used_at }}
+                                @else
+                                    {{ $voucher->status }}
+                                @endif
+                            </td>
+                            <td class="text-center">
+                                <a href="{{ route('admin.gift_vouchers.edit', $voucher) }}" class="btn btn-warning">
+                                    <i class="fa-solid fa-pencil"></i>
+                                </a>
+                                <form class="d-inline-block" action="{{ route('admin.gift_vouchers.destroy', $voucher) }}"
+                                    method="POST"
+                                    onsubmit="return confirm('Sei sicuro di voler cancellare questo voucher?')">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">
+                                        <i class="fa-regular fa-trash-can"></i>
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         @else
             <h2>Nessun buono regalo disponibile</h2>
         @endif
