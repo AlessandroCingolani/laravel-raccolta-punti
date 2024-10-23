73f1c0c91b56e6d02288322743178382b5700392
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-10-23 13:59:20 +0200

    [init]:Gift voucher controller with create

---
+++ app/Http/Controllers/Admin/GiftVouchersController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\GiftVoucherRequest;
+use Illuminate\Http\Request;
+use App\Models\GiftVoucher;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+
+class GiftVouchersController extends Controller
+{
+
+    const MONTH_VOUCHER_VALIDATION = 2;
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $vouchers = GiftVoucher::paginate(10);
+        return view('admin.gift_vouchers.index', compact('vouchers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $title = "Aggiungi buono regalo";
+        $method = 'POST';
+        $route = route('admin.gift_vouchers.store');
+        $gift_voucher = null;
+        $button = 'Crea nuovo buono regalo';
+
+        return view('admin.gift_vouchers.create-edit', compact('title', 'method', 'route', 'button', 'gift_voucher'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(GiftVoucherRequest $request)
+    {
+        $form_data = $request->all();
+
+        $uniqueCode = $this->generateUniqueCode();
+        $form_data['code'] = $uniqueCode;
+
+        // expiration time for coupon
+        $expirationDate = Carbon::now()->addMonths(self::MONTH_VOUCHER_VALIDATION);
+        $form_data['expiration_date'] = $expirationDate;
+
+        $new_gift_voucher = GiftVoucher::create($form_data);
+
+        return redirect()->route('admin.gift_vouchers.show', $new_gift_voucher);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(GiftVoucher $gift_voucher)
+    {
+        return view('admin.gift_vouchers.show', compact('gift_voucher'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    /**
+     * Genera un codice unico per il buono regalo
+     *
+     * @return string
+     */
+    private function generateUniqueCode()
+    {
+        // generate unique code voucher
+        $code = Str::random(15);
+
+        // if code exist generate another code
+        while (GiftVoucher::where('code', $code)->exists()) {
+            $code = Str::random(15);
+        }
+
+        return $code;
+    }
+}
