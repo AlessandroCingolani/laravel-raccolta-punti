5aa65b77c239319c570615370c7141b07d1fbebc
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-10-28 11:56:17 +0100

    [add]:api autocomplete gift

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
+use App\Models\GiftVoucher;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 
@@ -28,5 +29,25 @@ class PageController extends Controller
         $customers = $customers->take(5)
             ->get();
         return response()->json($customers);
+    }
+
+    // Search first name or last name on table gift voucher
+    public function autoCompleteGift($search)
+    {
+        $search = explode(' ', $search);
+
+        $gift_vouchers = GiftVoucher::query();
+        if (count($search) === 2) {
+            $firstName = $search[0];
+            $lastName = $search[1];
+            $gift_vouchers->where('recipient_first_name', 'LIKE', '%' . $firstName . '%')
+                ->orWhere('recipient_last_name', 'LIKE', '%' . $lastName . '%');
+        } else {
+            $gift_vouchers->where('recipient_first_name', 'LIKE', '%' . $search[0] . '%')
+                ->orWhere('recipient_last_name', 'LIKE', '%' . $search[0] . '%');
+        };
+        $gift_vouchers = $gift_vouchers->take(5)
+            ->get();
+        return response()->json($gift_vouchers);
     }
 }
