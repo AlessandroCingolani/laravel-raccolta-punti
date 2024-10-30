cc2e7343498ab5a78e6d0ebd34d8c38dff8f4cd7
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-10-30 15:21:16 +0100

    [add]:active style aside

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,6 +1,6 @@
 <aside>
 
-    <ul id="link-menu" class="text-start py-3 px-4 d-none d-md-block">
+    <ul id="link-menu" class="text-start py-3 px-0 d-none d-md-block">
         <li class="my-3">
             <a href="{{ route('dashboard') }}" @class(['active' => Route::is('dashboard')])>
                 Home</a>
@@ -48,7 +48,7 @@
         </li>
     </ul>
 
-    <ul id="link-menu-sm" class="text-center py-3 px-2 d-md-none">
+    <ul id="link-menu-sm" class="text-center py-3 px-0 d-md-none">
         <li class="my-3">
             <a href="{{ route('dashboard') }}" @class(['active' => Route::is('dashboard')])>
                 <i class="fa-solid fa-house"></i></a>
