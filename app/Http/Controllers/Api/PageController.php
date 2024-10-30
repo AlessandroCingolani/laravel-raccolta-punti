5e822f24519dedec32c9928ec992abb89fb3c946
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-10-30 11:31:35 +0100

    [add]:autocomplete api for customers used coupons

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Functions\Helper;
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use App\Models\GiftVoucher;
+use App\Models\Purchase;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 
@@ -49,5 +51,43 @@ class PageController extends Controller
         $gift_vouchers = $gift_vouchers->take(5)
             ->get();
         return response()->json($gift_vouchers);
+    }
+
+    public function autoCompleteCouponCustomers($search)
+    {
+        $search = explode(' ', $search);
+
+        // Query base per i clienti che hanno utilizzato i coupon
+        $purchasesQuery = Purchase::with('customer')
+            ->where('coupons_used', '>', 0)
+            ->whereBetween('created_at', Helper::getReferencePeriod());
+
+        // Filtra in base al nome e cognome del cliente
+        if (count($search) === 2) {
+            $firstName = $search[0];
+            $lastName = $search[1];
+            $purchasesQuery->whereHas('customer', function ($query) use ($firstName, $lastName) {
+                $query->where('name', 'LIKE', '%' . $firstName . '%')
+                    ->where('surname', 'LIKE', '%' . $lastName . '%');
+            });
+        } else {
+            $namePart = $search[0];
+            $purchasesQuery->whereHas('customer', function ($query) use ($namePart) {
+                $query->where('name', 'LIKE', '%' . $namePart . '%')
+                    ->orWhere('surname', 'LIKE', '%' . $namePart . '%');
+            });
+        }
+
+        // Ottiene i dati dei clienti in base ai risultati della query
+        $customers = $purchasesQuery->take(5)->get()->map(function ($purchase) {
+            return [
+                'name' => $purchase->customer->name,
+                'surname' => $purchase->customer->surname,
+                'email' => $purchase->customer->email,
+                'coupons_used' => $purchase->coupons_used,
+            ];
+        });
+
+        return response()->json($customers);
     }
 }
